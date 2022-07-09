9757da4fd3073bd8af5a2dc1d141dcc61bdcadba
tomihlt <tomshaulet@gmail.com>
2022-07-09 10:55:16 -0300

    operaciones basicas matrices v2

--- tp/src/main/java/died/izaguirre/haulet/tp/Matriz/Matriz.java
+++ tp/src/main/java/died/izaguirre/haulet/tp/Matriz/Matriz.java
@@ -1,60 +1,85 @@
 package died.izaguirre.haulet.tp.Matriz;
 
 public class Matriz {
-	
-	private int[][] valores; // Indican si hay camino, o el peso de los mismos (los pesos son valores enteros).
-	
-	public Matriz() {};
-	
+
+	private int[][] valores; // Indican si hay camino, o el peso de los mismos (los pesos son valores
+								// enteros).
+
+	public Matriz() {
+	};
+
 	public Matriz(int n) {
 		this();
-		valores = new int[n][n];		
+		valores = new int[n][n];
+	}
+
+	public Matriz(int[][] mat) {
+		valores = mat;
+	}
+
+	// No hago los metodos de como cargar datos hasta que definamos la clase de
+	// grafo.
+
+	public int[][] getValores() {
+		return valores;
+	}
+
+	// Metodos básicos -- Deberian usarse como auxiliar en esta clase nomas
+	private int[][] duplicarMatriz(int[][] mat) {
+
+		int[][] resultante = new int[mat.length][mat.length];
+
+		for (int i = 0; i < mat.length; i++)
+			for (int j = 0; j < mat.length; j++)
+				resultante[i][j] = mat[i][j];
+
+		return resultante;
+
+	}
+
+	private int[][] multiplicar(int[][] matA, int[][] matB) {
+
+		int[][] resultante = new int[matA.length][matB[0].length];
+
+		if (matA[0].length == matB.length) {
+			for (int i = 0; i < matA.length; i++) {
+				for (int j = 0; j < matB[0].length; j++) {
+					for (int k = 0; k < matA[0].length; k++) {
+						resultante[i][j] += matA[i][k] * matB[k][j];
+					}
+				}
+			}
+		}
+
+		return resultante;
+
+	}
+
+	private int[][] potencia(int[][] mat, int n) {
+
+		if (n <= 0)
+			return mat;
+		else if (n == 1)
+			return mat;
+		else
+			return multiplicar(mat, potencia(mat, n - 1));
+
 	}
 	
-	// No hago los metodos de como cargar datos hasta que definamos la clase de grafo.
-	
-	// Metodos básicos
-	private int[][] duplicarMatriz(int[][] mat) {
+	// Transforma una matriz de enteros a una booleana, util para la transitividad
+	private boolean[][] matrizBooleana(int[][] mat){
 		
-		int[][] resultante = new int[mat.length][mat.length];
+		boolean[][] resultante = new boolean[mat.length][mat.length];
 		
 		for(int i = 0 ; i < mat.length ; i++)
-			for(int j = 0 ; j < mat.length ; j++)
-				resultante[i][j] = mat[i][j];
+			for(int j = 0 ; j < mat.length ; j ++)
+				resultante[i][j] = mat[i][j] != 0 ? true : false;
 		
 		return resultante;
 		
 	}
 	
-	private int[][] multiplicar(int[][] matA, int[][] matB){
-		
-		int[][] resultante = new int[matA.length][matB[0].length];
-		
-		if (matA[0].length == matB.length) {
-	        for (int i = 0; i < matA.length; i++) {
-	            for (int j = 0; j < matB[0].length; j++) {
-	                for (int k = 0; k < matA[0].length; k++) {
-	                    // aquí se multiplica la matriz
-	                    resultante[i][j] += matA[i][k] * matB[k][j];
-	                }
-	            }
-	        }
-	    }
-		
-		return resultante;
-		
-	}
-	
-	private int[][] potencia(int[][] mat, int n){
-		
-		if(n <= 0)
-			return mat;
-		else if(n == 1)
-				return mat;
-			else
-				return multiplicar(mat, potencia(mat, n-1));
-		
-	}
+	// Algoritmo de warshall
 	
 	
 	
